cb347c5437d77c7760a40bbf09280b50a39141d8
Clay Juneau <clayjuneau@gmail.com>
2020-12-23 16:57:05 -0600

    Appointment form functionality

--- reactinterface/src/components/App.js
+++ reactinterface/src/components/App.js
@@ -9,6 +9,7 @@ import {without} from 'lodash';
 function App(){
 
   const [appointments, setAppointments] = useState([])
+  const [formDisplay, setFormDisplay] = useState(false)
 
   useEffect(() => {
     fetch('./data.json')
@@ -23,15 +24,32 @@ function App(){
     setAppointments(tempApts)
   }
 
+  function toggleForm() {
+    setFormDisplay(!formDisplay)
+  }
+
+  function addAppointment(apt) {
+    let tempApts = appointments
+    tempApts.unshift(apt)
+    setAppointments(tempApts)
+  }
+
   return (
     <main className="page bg-white" id="petratings">
       <div className="container">
         <div className="row">
           <div className="col-md-12 bg-white">
             <div className="container">
-              <AddAppointments />
+              <AddAppointments 
+                formDisplay={formDisplay} 
+                toggleForm={toggleForm}
+                addAppointment={addAppointment}
+              />
               <SearchAppointments />
-              <ListAppointments appointments={appointments} deleteAppointment={deleteAppointment}/>
+              <ListAppointments 
+                appointments={appointments} 
+                deleteAppointment={deleteAppointment}
+              />
             </div>
           </div>
         </div>
